c5036f5975be437a64cc530704f5f6f79cc8c495
zgg <45032427@qq.com>
2021-05-13 15:37:35 +0800

    初始化仓库

---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="index">
+		{{test}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				test: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
