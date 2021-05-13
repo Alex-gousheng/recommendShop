2c017c46e4f721c5b0ec170def3df06c3b1cf5b8
zgg <45032427@qq.com>
2021-05-13 18:22:04 +0800

    test

--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,31 +1,6 @@
 <template>
 	<view class="index">
-		<view class="box">
-			<view class="cu-bar tabbar margin-bottom-xl bg-black">
-				<view class="action text-orange">
-					<view class="cuIcon-homefill"></view> 首页
-				</view>
-				<view class="action text-gray">
-					<view class="cuIcon-similar"></view> 分类
-				</view>
-				<view class="action text-gray">
-					<view class="cuIcon-recharge"></view>
-					积分
-				</view>
-				<view class="action text-gray">
-					<view class="cuIcon-cart">
-						<view class="cu-tag badge">99</view>
-					</view>
-					购物车
-				</view>
-				<view class="action text-gray">
-					<view class="cuIcon-my">
-						<view class="cu-tag badge"></view>
-					</view>
-					我的
-				</view>
-			</view>
-		</view>
+		
 	</view>
 </template>
 
